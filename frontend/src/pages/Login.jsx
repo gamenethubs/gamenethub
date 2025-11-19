4b5f206fc90dd2c70cf3f02a73d987489bcac998
gamenethubs <gamenethubs@gmail.com>
2025-11-19 21:02:42 +0530

    google login

--- frontend/src/pages/Login.jsx
+++ frontend/src/pages/Login.jsx
@@ -678,7 +678,7 @@ export default function Login() {
     try {
       setLoading(true);
 
-      const res = await fetch(`${apiBaseURL}/auth/google-login`, {
+      const res = await fetch(`${apiBaseURL}/api/auth/google-login`, {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify({
