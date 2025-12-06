6b018f1faf3ebc4adce092959b3529a142581527
Nishant <43836369+NK19Coder@users.noreply.github.com>
2025-12-06 18:09:10 +0530

    bug

--- backend/routes/xpRoutes.js
+++ backend/routes/xpRoutes.js
@@ -1,6 +1,7 @@
 import express from "express";
 import { protect } from "../middleware/authMiddleware.js";
 import { getUserXPStats } from "../utils/xpSystem.js";
+import User from "../models/User.js";
 
 const router = express.Router();
 
