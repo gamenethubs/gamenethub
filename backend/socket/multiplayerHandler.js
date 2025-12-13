824715e637751017c1f9ef3e42c9b7733424788c
gamenethubs <gamenethubs@gmail.com>
2025-12-13 19:23:30 +0530

    dots game socket changes

--- backend/socket/multiplayerHandler.js
+++ backend/socket/multiplayerHandler.js
@@ -1,6 +1,7 @@
 // backend/socket/multiplayerHandler.js
 
 import tictactoe from "./games/tictactoe.js";
+import dotsAndBoxes from "./games/dotsAndBoxes.js";
 
 export default function multiplayerHandler(io) {
   
@@ -10,9 +11,14 @@ export default function multiplayerHandler(io) {
     tictactoe(socket, io);
   });
 
+  // Namespace for Dots & Boxes
+  io.of("/dots-and-boxes").on("connection", (socket) => {
+    console.log("🎲 New Dots & Boxes connection:", socket.id);
+    dotsAndBoxes(socket, io);
+  });
+
   // ⚠️ Future games can be added like this:
   // io.of("/ludo").on("connection", (socket) => {
   //   ludo(socket, io);
   // });
-
 }
